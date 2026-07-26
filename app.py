d12870e4307a180f74c763f3a9805e4f4c9eddec
Aleyna Baltaş <baltasaleyna3@gmail.com>
2026-07-26 23:35:33 +0300

    app.py

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ import plotly.express as px
 import sys
 import os
 
-# Proje kök dizinini path'e ekle (import için)
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from data_preprocessing import veriyi_yukle_ve_isle, NUMERIK_KRITERLER
 from decision_model import KararDestek, KRITER_AGIRLIKLARI
 
